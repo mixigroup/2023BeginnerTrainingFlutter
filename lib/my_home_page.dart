111136449de443b0945c6e0d73d528e7a3f2bc94
久野文菜 <ayana.kuno@mixi.co.jp>
2023-04-25 00:41:35 +0900

    add twice

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -18,12 +18,19 @@ class _MyHomePageState extends State<MyHomePage> {
   // この子が状態を持つデータ（今後 state と呼びます）
   // Tips: _ はプライベート変数を表してるよ
   int _counter = 0;
+  int _counter2 = 1;
 
-  void _incrementCounter() {
+  void _increment() {
     // state は setState() 内で更新させないと再描画されないよ！
     setState(() {
       // 右下のボタンが押されたら _counter が 0 からプラスされた状態になる
       _counter++;
+    });
+  }
+
+  void _twice() {
+    setState(() {
+      _counter2 = _counter2 * 2;
     });
   }
 
@@ -53,20 +60,38 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '足し算:',
             ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              '2倍されてく:',
+            ),
+            Text(
+              '$_counter2',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       // 右下のプラスボタン（Floating Action Button と言います）
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: _increment,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            onPressed: _twice,
+            tooltip: '3 times',
+            child: const Icon(Icons.close),
+          ),
+        ],
       ),
     );
   }
