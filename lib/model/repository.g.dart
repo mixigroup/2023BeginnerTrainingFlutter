bf3095cf177846fdb352637115764c05a3b5dd9a
久野文菜 <ayana.kuno@mixi.co.jp>
2023-04-25 14:01:45 +0900

    model 追加

---
+++ lib/model/repository.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Repository _$RepositoryFromJson(Map<String, dynamic> json) => Repository(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$RepositoryToJson(Repository instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
