1d0ebd122bb393766cb6c808bc8af6fba338a7d7
久野文菜 <ayana.kuno@mixi.co.jp>
2023-04-26 23:30:34 +0900

    toJson を使うように

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -1,9 +1,12 @@
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 part 'message.g.dart';
 
 // json の時と同じくモデルを作成！
 @HiveType(typeId: 0)
+// API に渡すために json に戻す必要があるので追加して generate
+@JsonSerializable()
 class MessageItem extends HiveObject {
   @HiveField(0)
   String content;
@@ -11,4 +14,8 @@ class MessageItem extends HiveObject {
   String role;
 
   MessageItem({required this.content, required this.role});
+
+  factory MessageItem.fromJson(Map<String, dynamic> json) =>
+      _$MessageItemFromJson(json);
+  Map<String, dynamic> toJson() => _$MessageItemToJson(this);
 }
