4d6d5dda8edf1712c27f1b5c9250fa70fe0428bb
KNO3 <jazzy.kno3.wonder@gmail.com>
2023-05-09 17:25:26 +0900

    Revert "TeamChellenge! Bloc で書き直してみよう"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:chat_sample/bloc/chat_bloc.dart';
 import 'package:chat_sample/chat_page.dart';
-import 'package:chat_sample/model/message.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -12,8 +9,6 @@ void main() async {
   await dotenv.load(fileName: '.env');
   // Hive の初期化
   await Hive.initFlutter();
-  // Adapter を使えるように
-  Hive.registerAdapter(MessageItemAdapter());
   // 下の MyApp を run するよ〜
   runApp(const MyApp());
 }
@@ -40,18 +35,15 @@ class MyApp extends StatelessWidget {
   // 画面遷移をする場合の状態監視，最初に表示させるページ `home: ` を指定しているよ
   @override
   Widget build(BuildContext context) {
-    // MaterialApp 内で bloc 使えるように
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ChatBloc>(create: (context) => ChatBloc()),
-      ],
-      child: MaterialApp(
-        title: 'Chat Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.amber,
-        ),
-        home: const ChatPage(title: 'Chat by GPT-3'),
+    return MaterialApp(
+      // OS のテーマ設定に合わせて変更できる
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
       ),
+      // 最初に表示させるページをは下の MyHomePage
+      // 引数として title 渡してる（無くてもいいよ）
+      home: const ChatPage(title: 'Chat by ChatGPT'),
     );
   }
 }
